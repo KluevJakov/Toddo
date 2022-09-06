ed42665c82e7fe360f33ab20877749a5c8894d57
KluevJakov <kluevja@gmail.com>
2022-09-06 04:11:31 +0200

    init app

---
+++ src/main/java/com/kluevja/toddo/ToddoApplication.java
@@ -0,0 +1,13 @@
+package com.kluevja.toddo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToddoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToddoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kluevja/toddo/ToddoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kluevja.toddo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToddoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
